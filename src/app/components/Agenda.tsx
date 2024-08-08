b9d1a590658b507ed5bf9ee9d39d245c15e3d6a9
carlos <carlos.boor@gmail.com>
2024-08-07 23:33:33 -0600

    update

--- src/app/components/Agenda.tsx
+++ src/app/components/Agenda.tsx
@@ -38,28 +38,36 @@ const Agenda: React.FC = () => {
     return (
         <div className="flex flex-col align-middle justify-center text-center pt-6 pb-12 bg-white shadow-md" id="agenda">
             <h2 className="text-2xl font-bold text-blue-700 mb-4">Agenda</h2>
-            <table className="w-full text-left border-collapse max-w-5xl m-auto">
-                <thead className="rounded-t-lg">
-                    <tr className="bg-blue-600 text-white">
-                        <th className="p-4 first:rounded-tl-lg last:rounded-tr-lg" style={{ width: '15%' }}>Horario</th>
-                        <th className="p-4 first:rounded-tl-lg last:rounded-tr-lg" style={{ width: '40%' }}>Ponencia</th>
-                        <th className="p-4 first:rounded-tl-lg last:rounded-tr-lg" style={{ width: '45%' }}>Panelista</th>
+            <table className="w-full text-center border-collapse max-w-5xl m-auto">
+                <thead >
+                    <tr className=" text-white mb-5">
+                        <th className="p-4 rounded-md bg-blue-600" style={{ width: '15%' }}>Horario</th>
+                        <th className="p-4 rounded-full" style={{ width: '2.5%' }}></th>
+                        <th className="p-4 rounded-md bg-blue-600" style={{ width: '40%' }}>Actividad</th>
+                        <th className="p-4 rounded-full" style={{ width: '2.5%' }}></th>
+                        <th className="p-4 rounded-md bg-blue-600" style={{ width: '35%' }}>Ponentes</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <tr className="bg-white ">
+                        <td className="p-3" colSpan={5}></td>
+                    </tr>
                     {items.map((item) => (
                         <tr
-                            key={item.id} // Usar el identificador único como clave.
-                            className="odd:bg-white even:bg-gray-100 hover:shadow-lg border-y-8 transition duration-300 ease-in-out transform hover:scale-105 text-blue-800 rounded-lg overflow-hidden"
+                            key={item.id} 
+                            className="rounded-full border-collapse hover:shadow-sm border-y-8 transition duration-300 ease-in-out transform hover:scale-105 text-blue-800 overflow-hidden"
                             style={{
-                                backgroundImage: 'radial-gradient(circle, rgba(35,126,235,0.8), rgba(173,216,230,0.2))',
+                                backgroundImage: 'radial-gradient(circle, rgba(0,111,207,0.5), rgba(102,169,226,0.2))',
                                 backgroundSize: '150% 150%',
                                 backgroundPosition: 'center',
+                                borderColor: 'white',
+                                borderRadius: '50px',
+                                margin: '8px 0',
                             }}
                         >
-                            <td className="p-4">{item.horario}</td>
-                            <td className="p-4">{item.ponencia}</td>
-                            <td className="p-4">{item.panelista}</td>
+                            <td className="p-2 " colSpan={2} style={{ borderEndStartRadius: 12, borderTopLeftRadius:12}}>{item.horario}</td>
+                            <td className="p-2" colSpan={2}>{item.ponencia}</td>
+                            <td className="p-2" style={{ borderEndEndRadius: 12, borderTopRightRadius:12}}>{item.panelista}</td>
                         </tr>
                     ))}
                 </tbody>
