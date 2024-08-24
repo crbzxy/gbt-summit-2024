3e51f05fe720f8472d7ad22ff4277015596a01a9
carlos <carlos.boor@gmail.com>
2024-08-23 18:12:59 -0600

    update to prod8!

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
-import dbConnect from '@/lib/dbConnect';
-import User from '@/models/User';
+import dbConnect from '../../../../lib/dbConnect';
+import User from '../../../../models/User';
 
 export async function GET(request: Request) {
   await dbConnect();
