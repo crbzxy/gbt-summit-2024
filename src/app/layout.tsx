bced8dfc0a0b018dd455cbc16e7dab9ec5496348
carlos <carlos.boor@gmail.com>
2024-09-08 15:37:50 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
+"use client"; // Esto indica que es un componente del lado del cliente
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Importamos el componente Script de Next.js
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+let metadata: Metadata = {
   title: "Amex GBT Summit 2024",
   description: "Amex GBT Summit 2024",
 };
@@ -16,7 +19,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es-mx">
-      <body className={inter.className}>{children}</body>
+      <head>
+        {/* Google Analytics Tag */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-NYWPJ79YRE`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NYWPJ79YRE');
+            `,
+          }}
+        />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
