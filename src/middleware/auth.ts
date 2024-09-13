11a5dfed602c3253552939a586eb833dea155e02
carlos <carlos.boor@gmail.com>
2024-09-13 00:05:33 -0600

    update 22

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
-import dbConnect from '../lib/dbConnect';
+import {dbConnect} from '@/lib/dbConnect';;
 import User from '../models/User';
 
 export async function middleware(request: NextRequest) {
